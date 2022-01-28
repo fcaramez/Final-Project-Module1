fd496eca5458807858126f010265c0267865b1b2
Francisco Duarte <francisco.caramez.duarte@gmail.com>
2022-01-28 15:05:28 +0000

    starts to sketch initial idea into code

---
+++ jScript/player.js
@@ -0,0 +1,9 @@
+class Player {
+    constructor() {
+        this.canvas = document.getElementById("game");
+        this.ctx = canvas.getContext("2d");
+        this.x = x;
+        this.y = y;
+        
+    }
+}
