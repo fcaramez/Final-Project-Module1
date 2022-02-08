75bcc0e1c28ad7497869a2d091a75f8bc1841c1e
Francisco Duarte <francisco.caramez.duarte@gmail.com>
2022-02-08 20:56:35 +0000

    solved a minor bug

--- jScript/player.js
+++ jScript/player.js
@@ -1,40 +1,40 @@
 class Player {
-    constructor(game, x, y, width, height) {
-        this.game = game;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.img = new Image();
-    }
+  constructor(game, x, y, width, height) {
+    this.game = game;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.img = new Image();
+  }
 
-    left() {
-        return this.x;
-    }
+  left() {
+    return this.x;
+  }
 
-    right() {
-        return this.x + this.width;
-    }
+  right() {
+    return this.x + this.width;
+  }
 
-    top() {
-        return this.y;
-    }
+  top() {
+    return this.y;
+  }
 
-    bottom() {
-        return this.y + this.height;
-    }
+  bottom() {
+    return this.y + this.height;
+  }
 
-    colision(enemy) {
-        return !(
-            this.bottom() < enemy.top() ||
-            this.top() > enemy.bottom() ||
-            this.right() < enemy.left() ||
-            this.left() > enemy.right()
-          );
-    }
+  colision(enemy) {
+    return !(
+      this.bottom() < enemy.top() ||
+      this.top() > enemy.bottom() ||
+      this.right() < enemy.left() ||
+      this.left() > enemy.right()
+    );
+  }
 
-    draw() {
-        this.img.src = './docs/assets/Player-removebg-preview.png';
-        this.game.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
+  draw() {
+    this.img.src = "./docs/assets/Player-removebg-preview.png";
+    this.game.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
 }
--- jScript/scoreUp.js
+++ jScript/scoreUp.js
@@ -1,28 +1,30 @@
 class ScoreUp {
-    constructor(game) {
-        this.game = game;
-        this. x = Math.floor(Math.random() * 980);
-        this.y = Math.floor(Math.random() * 630);
-        this.powerUp = new Image();
-    }
+  constructor(game) {
+    this.game = game;
+    this.x = Math.floor(Math.random() * 980);
+    this.y = Math.floor(Math.random() * 630);
+    this.width = 80;
+    this.height = 80;
+    this.powerUp = new Image();
+  }
 
-    drawPowerUp() {
-        this.powerUp.src = './docs/assets/powerUp-removebg-preview.png';
-        this.game.ctx.drawImage(this.powerUp, this.x, this.y, 80, 80);
-    }
+  drawPowerUp() {
+    this.powerUp.src = "./docs/assets/powerUp-removebg-preview.png";
+    this.game.ctx.drawImage(this.powerUp, this.x, this.y, 80, 80);
+  }
 
-    left() {
-        return this.x;
-    }
-    right() {
-        return this.x + this.width;
-    }
-    
-    top() {
-        return this.y;
-    }
-    
-    bottom() {
-        return this.y + this.height;
-    }
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+
+  top() {
+    return this.y;
+  }
+
+  bottom() {
+    return this.y + this.height;
+  }
 }
